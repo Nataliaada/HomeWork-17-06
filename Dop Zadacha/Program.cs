591f66c22509f979061fd856e12e7b72bc2b3abc
Nataliaada <106996389+Nataliaada@users.noreply.github.com>
2022-06-20 17:26:41 +0300

    Упростила команду

--- Dop Zadacha/Program.cs
+++ Dop Zadacha/Program.cs
@@ -2,4 +2,4 @@
 
 Console.WriteLine("Введите число:");
 int a = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine($"Модуль числа {Math.Abs(a)}");
+Console.WriteLine($"Модуль числа {a} = {Math.Abs(a)}");
