dac2140bcbf70974dc48aa0a8df32eaf003bca37
Nataliaada <106996389+Nataliaada@users.noreply.github.com>
2022-06-20 15:49:33 +0300

    Save all

---
+++ Dop Zadacha/Program.cs
@@ -0,0 +1,6 @@
+﻿// Напишите программу вычисления модуля числа. 2 -> 2  - 3 -> 3  -7 -> 7
+
+Console.WriteLine("Введите число:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Модуль числа = ");
+Console.WriteLine(Math.Abs(a));
---
+++ HOME_4/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 4: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа 
+//от 1 до N. 5 -> 2, 48 -> 2, 4, 6, 8
+
+
+Console.WriteLine("Введите число:");
+int b = Convert.ToInt32(Console.ReadLine());
+int i = 0;
+// Console.WriteLine($"Чётные числа из диапазона от 1 до {b}:");
+
+while (i < b) 
+{
+i++;
+if (i%2 == 0) 
+Console.WriteLine($"{i} ");
+}
+Console.WriteLine();
